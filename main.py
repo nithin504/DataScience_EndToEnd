ff355a9087ba1b5fed27200cca175a5b5f515680
nithin504 <nithinkumarsargadam@gmail.com>
2025-07-04 14:17:28 +0530

    data ingestion commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.datascience import logger
-
+from src.datascience.pipeline.data_ingestionPipeline import DataIngestionTainingPipeline
 logger.info("Logging has been set up successfully.")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj=DataIngestionTainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
