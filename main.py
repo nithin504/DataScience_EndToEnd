e425b7313c4f237642f0e3608f82f6c62ff5c662
nithin504 <nithinkumarsargadam@gmail.com>
2025-07-04 19:37:38 +0530

    model evaluation commited

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.datascience.pipeline.data_ingestionPipeline import DataIngestionTrainin
 from src.datascience.pipeline.data_validationPipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformationPipeline import DataTransformationTrainingPipeline
 from src.datascience.pipeline.model_trainerPipeline import ModelTrainerPipeline
+from src.datascience.pipeline.model_evaluationPipeline import ModelEvaluationPipeline
 logger.info("Logging has been set up successfully.")
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -41,6 +42,16 @@ try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         model_trainer=ModelTrainerPipeline()
         model_trainer.initiate_model_trainer()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model evaluation Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_evaluation=ModelEvaluationPipeline()
+        model_evaluation.initiate_model_evaluation()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/datascience/components/model_evaluation.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import joblib
+from src.datascience.entity.config_entity import ModelEvaluationConfig
+from src.datascience.utils.common import save_json
+from pathlib import Path
+
+
+class ModelEvaluation:
+    def __init__(self, config: ModelEvaluationConfig):
+        self.config = config
+
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+    
+    def log_into_mlflow(self):
+
+        test_data = pd.read_csv(self.config.test_data_path)
+        model = joblib.load(self.config.model_path)
+
+        test_x = test_data.drop([self.config.target_column], axis=1)
+        test_y = test_data[[self.config.target_column]]
+
+        os.environ["MLFlOW_TRACKING_URI"]="https://dagshub.com/nithin504/DataScience_EndToEnd.mlflow"
+        os.environ["MLFLOW_TRACKING_USERNAME"]="nithin504"
+        os.environ["MLFLOW_TRACKING_PASSWORD"]="8a049ffa54576c2a301a7fbee0a57f7ad55fbdc5"
+        os.environ["MLFLOW_RECORD_ENV_VARS_IN_MODEL_LOGGING"] = "false"
+
+
+        mlflow.set_registry_uri(self.config.mlflow_uri)
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        with mlflow.start_run():
+
+            predicted_qualities = model.predict(test_x)
+
+            (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
+            
+            # Saving metrics as local
+            scores = {"rmse": rmse, "mae": mae, "r2": r2}
+            save_json(path=Path(self.config.metric_file_name), data=scores)
+
+            mlflow.log_params(self.config.all_params)
+
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_metric("r2", r2)
+            mlflow.log_metric("mae", mae)
+
+
+            # Model registry does not work with file store
+            if tracking_url_type_store != "file":
+                mlflow.sklearn.log_model(sk_model=model, name="model",input_example=test_x.sample(n=5),registered_model_name="ElasticnetModel")
+            else:
+                mlflow.sklearn.log_model(sk_model=model,name="model",input_example=test_x.sample(n=5))
+    
